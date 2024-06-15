b24cb825d77e11bca8413dd2daa4dd5a4c1f1235
Fab <fabiomiguelchenhuang@gmail.com>
2024-06-15 13:49:37 +0100

    feat: added conditions to survey popup

--- src/components/SurveyPopup/SurveyPopup.tsx
+++ src/components/SurveyPopup/SurveyPopup.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./SurveyPopup.css";
+import { MdOutlineTimer } from "react-icons/md";
+import { RiGitRepositoryPrivateFill } from "react-icons/ri";
 
 const SurveyPopup = () => {
   const [isOpen, setIsOpen] = useState(true);
@@ -20,6 +22,17 @@ const SurveyPopup = () => {
             Participate in our study on Visual Regression Testing to share your
             thoughts, and help improve the existing tools and techniques.
           </p>
+          <div className="survey-popup-conditions">
+            <div className="survey-popup-icons">
+              <MdOutlineTimer />
+              <p>2-5 min</p>
+            </div>
+
+            <div className="survey-popup-icons">
+              <RiGitRepositoryPrivateFill />
+              <p>Anonymous</p>
+            </div>
+          </div>
           <a
             className="survey-popup-link"
             href="https://docs.google.com/forms/d/e/1FAIpQLSdDQvZhmvPtiNd-uHi3CvVMs5nEwklO71upTW1rYTIYPiboEA/viewform"
